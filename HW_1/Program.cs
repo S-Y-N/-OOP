574c119e7c6660630e9089da06b55dd40c6cb79b
S-Y-N <84926168+S-Y-N@users.noreply.github.com>
2021-10-26 12:05:09 +0300

    Add files via upload

---
+++ HW_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ты жив, здоров, беспечен, пей пока\n");
+            Console.Write("С красавицей, как роза цветника,\n");
+            Console.Write("Покамест не сорвет дыханье смерти\n");
+            Console.Write("Твой краткий век подобьем лепестка.\n");
+            Console.Write("\n\t\t\tОмар Хаям\n");
+        }
+    }
+}
+
+
+
