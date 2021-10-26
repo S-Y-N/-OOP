381452b9211d5c7d04df1ba3a8d5d7031e5dbc93
S-Y-N <84926168+S-Y-N@users.noreply.github.com>
2021-10-26 12:06:20 +0300

    Add files via upload

---
+++ HW_5/Three_Capitals.cs
@@ -0,0 +1,63 @@
+﻿/*
+ Разработать приложение, где сравнивается население 3 столиц.
+Страны обозначаются пространством имен, а город - классом в данном пространстве
+
+ */
+
+using System;
+
+namespace Ukraine
+{
+    class Kyiv
+    {
+        private static string _name = "Киев";
+        private static ulong _population = 2952499;
+
+        public static string Info()
+        {
+            return $"Столица Украины  - {_name}, Население {_population} человек";
+        }
+      
+    }
+}
+namespace USA
+{
+    class WashingtonDC
+    {
+        private static string _name = "Вашингтон";
+        private static ulong _population = 601723;
+
+        public static string Info()
+        {
+            return $"Столица США  - {_name}, Население {_population} человек";
+        }
+    }
+}
+namespace Island
+{
+    class Reykjavík
+    {
+        private static string _name = "Рейкьявик";
+        private static ulong _population = 368590;
+
+        public static string Info()
+        {
+            return $"Столица США  - {_name}, Население {_population} человек";
+        }
+    }
+}
+namespace Three_Capitals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(Ukraine.Kyiv.Info());
+            Console.WriteLine(USA.WashingtonDC.Info());
+            Console.WriteLine(Island.Reykjavík.Info());
+
+            Console.ReadKey();
+        }
+    }
+}
