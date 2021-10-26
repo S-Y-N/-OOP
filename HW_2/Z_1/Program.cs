881cf4ba3a8c616f887972573d85f3c527524608
S-Y-N <84926168+S-Y-N@users.noreply.github.com>
2021-10-26 12:05:32 +0300

    Add files via upload

---
+++ HW_2/Z_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ ДЗ-2 Си-шарп
+ Задание 1. 
+Даны целые числа А и В ( А < B).
+Вывести все целые числа от А до В включетельно. Выводить каждле число с новой строки и к-во = числу.
+
+пример вывода 
+1
+2 2
+3 3 3
+
+Тристан Евгений
+ПВ-011
+ */
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            int i, j;
+            Console.WriteLine("Введите два числа. Числа не могут быть меньше нуля");
+            Console.WriteLine("Введите число А: ");
+            A = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine("Введите число B: ");
+            B = Convert.ToUInt16(Console.ReadLine());
+            Console.Write("\nResult: \n");
+            for ( i = A; i <= B; i++)
+            {
+                for( j=1; j<=i; j++)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");                
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW_2/Z_2 LuckyTicket/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ ДЗ -2
+Задание 2
+Ввести с клавиатуры номер билета(6-ное число) и проверить его на удачу. Если сумма первых трех чисел
+равна сумме второй тройки чисел - счастливый.
+
+Тристан Евгений
+ПВ-011
+ */
+namespace СчастливыйБилетик_Сшарп
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер трамвайного билета:(6 цифр) ");
+            string str = Console.ReadLine();
+            char[] ch = new char[str.Length];
+            ch = str.ToCharArray();
+            int[] ticket = ch.Select(s => int.Parse(s.ToString())).ToArray();
+            if (ch.Length == 6)
+            {
+                int firstNumber = 0;
+                int secondNumber = 0;
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    if (i < 3)
+                    {
+                        firstNumber += ticket[i];
+                    }
+                    else secondNumber += ticket[i];
+                }
+                if (firstNumber == secondNumber)
+                {
+                    Console.WriteLine("У вас счастливый билетик: {0} = {1}", firstNumber, secondNumber);
+                }
+                else
+                {
+                    Console.WriteLine("У вас не счастливый билетик: {0} !={1}", firstNumber, secondNumber);
+                }
+            }
+            else
+                Console.WriteLine("Ошибка ввода числа!");
+            Console.ReadKey();
+        }
+    }
+}
